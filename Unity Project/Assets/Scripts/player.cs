256b5fcf4ebddcc0dc8d9af8b8f8a9124fb02c7a
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2019-12-24 21:35:01 +0800

    角色控制

---
+++ Unity Project/Assets/Scripts/player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+	[Header("速度"), Range(0, 1500)]
+	public float speed;
+
+	Rigidbody rig;           //剛體
+
+	FixedJoystick joystick; //虛擬搖桿
+
+	Animator anim;
+	
+	private void Start()
+	{
+
+		rig = GetComponent<Rigidbody>();
+		//GameObject.Find("物件名稱").GetComponent<T>();		T --泛型(任一個形式
+		joystick = GameObject.Find("虛擬搖桿").GetComponent<FixedJoystick>();
+		anim = GetComponent<Animator>();
+	}
+
+	/// <summary>
+	/// 固定執行1秒50次
+	/// </summary>
+	private void FixedUpdate()
+	{
+		Move();
+	}
+
+	/// <summary>
+	/// 移動方法
+	/// </summary>
+	private void Move()
+	{
+
+		float h = joystick.Horizontal;
+		float v = joystick.Vertical;
+		
+		
+		rig.AddForce(-h*speed,0,-v*speed);
+		
+		anim.SetBool("跑步開關", h != 0 || v != 0);
+			
+	}
+
+}
