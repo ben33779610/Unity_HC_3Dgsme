4df4e0722f079b38be1773c7cea678fdbe820a66
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2020-01-07 20:44:57 +0800

    光源 開門 切換場景

---
+++ Unity Project/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+	public GameObject skill;			 
+	public GameObject objlight;
+	[Header("自動開門")]
+	public bool autodoor;
+	[Header("自動顯示技能")]
+	public bool autoskill;
+
+	private Animator animdoor;
+
+
+	private void Start()
+	{
+		animdoor = GameObject.Find("門板").GetComponent<Animator>();
+		if (autoskill) Showskill();
+		if (autodoor) Invoke("Showdoor", 3);
+
+	}
+
+	/// <summary>
+	/// 顯示技能
+	/// </summary>
+	private void Showskill()
+	{
+		skill.SetActive(true);
+	}
+
+	/// <summary>
+	/// 開門 燈光
+	/// </summary>
+	private void Showdoor()
+	{
+		objlight.SetActive(true);
+		animdoor.SetTrigger("開門開關");
+	}
+
+}
